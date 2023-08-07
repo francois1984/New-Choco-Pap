80d051750775914103ea63305ecbfe5e5cdea4f0
François SDL <mhous4923@hotmail.com>
2023-08-07 19:41:19 +0200

    mise à jour avec Xavier test pour hébergement.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { CartProvider } from './components/CartContext'; 
 import { Accueil } from './screens/Accueil';
 import { Boutique } from './screens/Boutique';
@@ -14,32 +14,26 @@ import { Detail8 } from './screens/Detail8';
 import { Detail9 } from './screens/Detail9';
 import { Detail10 } from './screens/Detail10';
 
-
-
 function App() {
   return (
-
-    
-    <div>
+    <div >
       <CartProvider>
-        <Routes>
-          <Route path='/' element={<Accueil/>}/>
-          <Route path='/boutique' element={<Boutique/>}/>
-          <Route path='/detail1' element={<Detail1/>}/>
-          <Route path='/detail2' element={<Detail2/>}/>
-          <Route path='/detail3' element={<Detail3/>}/>
-          <Route path='/detail4' element={<Detail4/>}/>
-          <Route path='/detail5' element={<Detail5/>}/>
-          <Route path='/detail6' element={<Detail6/>}/>
-          <Route path='/detail7' element={<Detail7/>}/>
-          <Route path='/detail8' element={<Detail8/>}/>
-          <Route path='/detail9' element={<Detail9/>}/>
-          <Route path='/detail10' element={<Detail10/>}/>
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Accueil/>}/>
+            <Route path='/boutique' element={<Boutique/>}/>
+            <Route path='/detail1' element={<Detail1/>}/>
+            <Route path='/detail2' element={<Detail2/>}/>
+            <Route path='/detail3' element={<Detail3/>}/>
+            <Route path='/detail4' element={<Detail4/>}/>
+            <Route path='/detail5' element={<Detail5/>}/>
+            <Route path='/detail6' element={<Detail6/>}/>
+            <Route path='/detail7' element={<Detail7/>}/>
+            <Route path='/detail8' element={<Detail8/>}/>
+            <Route path='/detail9' element={<Detail9/>}/>
+            <Route path='/detail10' element={<Detail10/>}/>
+          </Routes>
       </CartProvider>
     </div>
-   
-    
   );
 }
 
